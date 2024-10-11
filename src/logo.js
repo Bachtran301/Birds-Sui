db1b6158f0778ad518837e8dbb7f25774a97f397
bachtran <bachtranduy2003@gmail.com>
2024-10-12 01:32:02 +0700

    first commit

---
+++ src/logo.js
@@ -0,0 +1,14 @@
+function printLogo(){
+    console.log(`
+    ██████╗ ██╗   ██╗████████╗██╗ ██████╗ ██████╗  ██████╗ ██╗      
+    ██╔══██╗██║   ██║╚══██╔══╝██║██╔════╝██╔═══██╗██╔═══██╗██║      
+    ██████╔╝██║   ██║   ██║   ██║██║     ██║   ██║██║   ██║██║      
+    ██╔═══╝ ██║   ██║   ██║   ██║██║     ██║   ██║██║   ██║██║      
+    ██║     ╚██████╔╝   ██║   ██║╚██████╗╚██████╔╝╚██████╔╝███████╗ 
+    ╚═╝      ╚═════╝    ╚═╝   ╚═╝ ╚═════╝ ╚═════╝  ╚═════╝ ╚══════╝ 
+            `.cyan);
+    console.log('[+] Welcome & Enjoy Sir !'.green);
+    console.log('[+] Error? PM Telegram [https://t.me/NothingYub]'.red);
+    console.log('[+] 📣 New Feature Update 📣  [https://t.me/puticoolbot]'.blue);
+}
+module.exports = printLogo;
